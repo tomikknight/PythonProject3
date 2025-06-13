c1becbd84f80a716da214fa10c4c5735abb1a726
Вадим <vdpodolski@gmail.com>
2025-06-13 22:21:39 +0300

    1)   1. Добавил Pygame и создал начальное окно. 2)   1. Добавил спрайт.   2. Добавил передвижение персонажа(пока не проверял со спрайтом). 3)   1. Проверил: персонаж не появляется.   2. В прошлой версии был чёрный фон - теперь белый. 4)   1. Исправил весь код теперь он работает: спрайтом можно управлять вверх, вниз, влево и вправо(пока у него нет пола).

--- main.py
+++ main.py
@@ -1,59 +1,77 @@
 import pygame as pg
+from random import *
 
 pg.init()
-width = 1024
-height = 800
-surface = pg.display.set_mode((width, height))
+pg.font.init()
 clock = pg.time.Clock()
-kni = 'knight/knight_stand.png'
-move_left1 = False
-move_right1 = False
-move_up1 = False
-move_down1 = False
+
+
+width = 3820
+height = 2460
+white = (255, 255, 255)
+black = (0, 0, 0)
+green = (34, 177, 76)
+yellow = (255, 242, 0)
+FPS = 60
+FONT = pg.font.SysFont('Arial', 35)
+surface = pg.display.set_mode((width, height))
+
+
+
+knight_img = pg.image.load('knight/knight_stand.png')
+knight_rect = knight_img.get_rect()
+knight_rect.x = width // 2
+knight_rect.y = height // 2
+
+move_left = False
+move_right = False
+move_up = False
+move_down = False
 speed = 5
-
-
 
 while True:
     for event in pg.event.get():
         if event.type == pg.QUIT:
+            pg.quit()
             exit()
         elif event.type == pg.KEYDOWN:
-            if event.key == pg.K_LEFT:
-                move_left1 = True
-            elif event.key == pg.K_RIGHT:
-                move_right1 = True
-            elif event.key == pg.K_UP:
-                move_up1 = True
-            elif event.key == pg.K_DOWN:
-                move_down1 = True
-
+            if event.key == pg.K_d:
+                move_right = True
+            elif event.key == pg.K_a:
+                move_left = True
+            elif event.key == pg.K_w:
+                move_up = True
+            elif event.key == pg.K_s:
+                move_down = True
         elif event.type == pg.KEYUP:
-            if event.key == pg.K_LEFT:
-                move_left1 = False
-            elif event.key == pg.K_RIGHT:
-                move_right1 = False
-            elif event.key == pg.K_UP:
-                move_up1 = False
-            elif event.key == pg.K_DOWN:
-                move_down1 = False
-    surface.fill(('#FFFFFF'))
+            if event.key == pg.K_d:
+                move_right = False
+            elif event.key == pg.K_a:
+                move_left = False
+            elif event.key == pg.K_w:
+                move_up = False
+            elif event.key == pg.K_s:
+                move_down = False
 
 
-    if move_left1 and kni > 0:
-        kni -= speed
-    elif move_right1 and kni + 40 < width:
-        kni += speed
-    elif move_up1 and kni > 0:
-        kni -= speed
-    elif move_down1 and kni + 70 < height:
-        kni += speed
+    surface.fill(white)
 
 
+    if move_left and knight_rect.x > 0:
+        knight_rect.x -= speed
+    if move_right and knight_rect.x + knight_rect.width < width:
+        knight_rect.x += speed
+    if move_up and knight_rect.y > 0:
+        knight_rect.y -= speed
+    if move_down and knight_rect.y + knight_rect.height < height:
+        knight_rect.y += speed
 
-    clock.tick(60)
+
+    surface.blit(knight_img, knight_rect)
+
+
+    clock.tick(FPS)
     pg.display.update()
-
 
 
 
