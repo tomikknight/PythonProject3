e688c145e5fcec6d482610116668a8e95abac617
Вадим <vdpodolski@gmail.com>
2025-06-13 14:51:08 +0300

    1)   1. Добавил Pygame и создал начальное окно.

--- main.py
+++ main.py
@@ -4,22 +4,54 @@ pg.init()
 width = 1024
 height = 800
 surface = pg.display.set_mode((width, height))
-
 clock = pg.time.Clock()
-pg.font.init()
 
-text_font = pg.font.SysFont('Arial', 30)
+kni = 'knight/knight_stand.png'
+move_left1 = False
+move_right1 = False
+move_up1 = False
+move_down1 = False
+speed = 5
+
 
 while True:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             exit()
-    surface.fill('#FFFFFF')
+        elif event.type == pg.KEYDOWN:
+            if event.key == pg.K_LEFT:
+                move_left1 = True
+            elif event.key == pg.K_RIGHT:
+                move_right1 = True
+            elif event.key == pg.K_UP:
+                move_up1 = True
+            elif event.key == pg.K_DOWN:
+                move_down1 = True
 
+        elif event.type == pg.KEYUP:
+            if event.key == pg.K_LEFT:
+                move_left1 = False
+            elif event.key == pg.K_RIGHT:
+                move_right1 = False
+            elif event.key == pg.K_UP:
+                move_up1 = False
+            elif event.key == pg.K_DOWN:
+                move_down1 = False
+
+
+    if move_left1 and kni > 0:
+        kni -= speed
+    elif move_right1 and kni + 40 < width:
+        kni += speed
+    elif move_up1 and kni > 0:
+        kni -= speed
+    elif move_down1 and kni + 70 < height:
+        kni += speed
+
+    pg.fill(255, 255, 255)
 
     clock.tick(60)
     pg.display.update()
-
 
 
 
