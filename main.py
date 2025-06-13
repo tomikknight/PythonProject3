4fd026bedc25dc8dee22e3c1905610995a4d3563
Вадим <vdpodolski@gmail.com>
2025-06-13 13:31:33 +0300

    1)   1. Добавил Pygame и создал начальное окно

--- main.py
+++ main.py
@@ -1,8 +1,38 @@
-f = 2
+import pygame as pg
+
+pg.init()
+width = 1024
+height = 800
+surface = pg.display.set_mode((width, height))
+
+clock = pg.time.Clock()
+pg.font.init()
+
+text_font = pg.font.SysFont('Arial', 30)
+
+while True:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            exit()
+    surface.fill('#FFFFFF')
+
+
+    clock.tick(60)
+    pg.display.update()
 
 
 
-print(5 + f)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
