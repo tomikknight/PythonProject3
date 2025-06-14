1a0d2e4452768b222b738a6d81a395c6e5ddff16
Вадим <vdpodolski@gmail.com>
2025-06-14 14:08:50 +0300

    1)   1. Добавил Pygame и создал начальное окно. 2)   1. Добавил спрайт.   2. Добавил передвижение персонажа(пока не проверял со спрайтом). 3)   1. Проверил: персонаж не появляется.   2. В прошлой версии был чёрный фон - теперь белый. 4)   1. Исправил весь код теперь он работает: спрайтом можно управлять вверх, вниз, влево и вправо(пока у него нет пола). 5)   1. Исправил спрайт, теперь всё работает. 6)   1. Добавил 2 спрайт.   2. Пытаюсь чтобы в зависимости от нажатия клавиш он применял разные спрайты для одного персонажа(пока не получается).

--- main.py
+++ main.py
@@ -1,78 +1,82 @@
 import pygame as pg
-from random import *
+from random import randint
+
 
 pg.init()
 pg.font.init()
 clock = pg.time.Clock()
 
 
-width = 1200
-height = 800
-white = (255, 255, 255)
-black = (0, 0, 0)
-green = (34, 177, 76)
-yellow = (255, 242, 0)
+WIDTH, HEIGHT = 1800, 1000
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GREEN = (34, 177, 76)
+YELLOW = (255, 242, 0)
 FPS = 60
 FONT = pg.font.SysFont('Arial', 35)
-surface = pg.display.set_mode((width, height))
+surface = pg.display.set_mode((WIDTH, HEIGHT))
 
 
+kni_le_sta_img = pg.image.load('knight/left/knight_stand_l.png')
+kni_ri_sta_img = pg.image.load('knight/right/knight_stand_r.png')
 
-knight_img = pg.image.load('knight/knight_stand.png')
-knight_rect = knight_img.get_rect()
-knight_rect.x = width // 2
-knight_rect.y = height // 2
+
+kni_l_s_rect = kni_le_sta_img.get_rect()
+kni_l_s_rect.x = WIDTH // 2
+kni_l_s_rect.y = HEIGHT // 2
+
+kni_r_s_rect = kni_ri_sta_img.get_rect()
+kni_r_s_rect.x = WIDTH // 2
+kni_r_s_rect.y = HEIGHT // 2
+
+
+speed = 6
 
 move_left = False
 move_right = False
-move_up = False
-move_down = False
-speed = 5
 
 while True:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             pg.quit()
             exit()
+
         elif event.type == pg.KEYDOWN:
             if event.key == pg.K_d:
                 move_right = True
             elif event.key == pg.K_a:
                 move_left = True
-            elif event.key == pg.K_w:
-                move_up = True
-            elif event.key == pg.K_s:
-                move_down = True
+
         elif event.type == pg.KEYUP:
             if event.key == pg.K_d:
                 move_right = False
             elif event.key == pg.K_a:
                 move_left = False
-            elif event.key == pg.K_w:
-                move_up = False
-            elif event.key == pg.K_s:
-                move_down = False
 
 
-    surface.fill(white)
+    if move_right:
+        if kni_r_s_rect.x + kni_ri_sta_img.get_rect().width + speed < WIDTH:
+            kni_r_s_rect.x += speed
+    elif move_left:
+        if kni_l_s_rect.x - speed > 0:
+            kni_l_s_rect.x -= speed
 
 
-    if move_left and knight_rect.x > 0:
-        knight_rect.x -= speed
-    if move_right and knight_rect.x + knight_rect.width < width:
-        knight_rect.x += speed
-    if move_up and knight_rect.y > 0:
-        knight_rect.y -= speed
-    if move_down and knight_rect.y + knight_rect.height < height:
-        knight_rect.y += speed
+    surface.fill(WHITE)
+    if move_right:
+        surface.blit(kni_ri_sta_img, kni_r_s_rect)
+    elif move_left:
+        surface.blit(kni_le_sta_img, kni_l_s_rect)
 
 
-    surface.blit(knight_img, knight_rect)
+    if kni_r_s_rect.x + kni_ri_sta_img.get_rect().width + speed < WIDTH:
+        surface.blit(kni_ri_sta_img, kni_r_s_rect)
+    else:
+        kni_l_s_rect.x = WIDTH - kni_ri_sta_img.get_rect().width
+        surface.blit(kni_ri_sta_img, kni_l_s_rect)
 
-
-    clock.tick(FPS)
     pg.display.update()
-
+    clock.tick(FPS)
 
 
 
