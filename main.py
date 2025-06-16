ddaeaeb93167a4bfeddf206e57be8aa5777bc48b
Вадим <vdpodolski@gmail.com>
2025-06-16 14:30:46 +0300

    Пробую сделать 1 локацию + управление из Hollow Knight в PyGame.  №2 1)   1. Добавил Pygame и создал начальное окно. 2)   1. Добавил спрайт.   2. Добавил передвижение персонажа(пока не проверял со спрайтом). 3)   1. Проверил: персонаж не появляется.   2. В прошлой версии был чёрный фон - теперь белый. 4)   1. Исправил весь код теперь он работает: спрайтом можно управлять вверх, вниз, влево и вправо(пока у него нет пола). 5)   1. Исправил спрайт, теперь всё работает. 6)   1. Добавил 2 спрайт.   2. Пытаюсь сделать так, чтобы в зависимости от нажатия клавиш он применял разные спрайты для одного персонажа(пока не получается).  7)   1. исправил код и теперь персонаж может двигаться вправо влево и изменяются спрайты.  8)   1. Исправил недочёт, когда при окончании передвижения он поворачивался в противоположную сторону.   2. Добавил спрайт пола.   3. ДОбавил сам пол и попытался сделать его бесконечным(пока не получилось). 9)   1. Добавил 3 спрайта пола и сделал их за экраном. 10)   1. Начал делать прыжок.   2. Спрайт чуть чуть поднимается, но пока не опускается.   3. Доделал прыжок + гравитация при падении.   4. Всё подписал.

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import pygame as pg
 from random import randint
 
+
 pg.init()
 pg.font.init()
 clock = pg.time.Clock()
 
+# настройки
 WIDTH, HEIGHT = 1800, 1000
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -14,6 +16,7 @@ FPS = 60
 FONT = pg.font.SysFont('Arial', 35)
 surface = pg.display.set_mode((WIDTH, HEIGHT))
 
+# загрузка спрайтов
 kni_le_sta_img = pg.image.load('images/knight/left/knight_stand_l.png').convert_alpha()
 kni_ri_sta_img = pg.image.load('images/knight/right/knight_stand_r.png').convert_alpha()
 floor_img = pg.image.load('images/no knight/floor/floor1.png')
@@ -21,35 +24,39 @@ floor2_2img = pg.image.load('images/no knight/floor/floor2_2.png')
 floor2_1img = pg.image.load('images/no knight/floor/floor2_1.png')
 floor2_3img = pg.image.load('images/no knight/floor/floor2_3.png')
 
-
+# спрайт персонажа
 current_sprite = kni_ri_sta_img
 sprite_rect = current_sprite.get_rect()
 sprite_rect.x = WIDTH // 2.05
 sprite_rect.y = HEIGHT // 10
 
+# земля
 floor_rect = floor_img.get_rect()
 floor_rect.x = 200
 floor_rect.y = HEIGHT - floor_img.get_rect().height + 85
+ground_y = 772
 
-floor2_1rect = floor2_1img.get_rect()
-floor2_1rect.x = -530
-floor2_1rect.y = HEIGHT - floor2_1img.get_rect().height + 70
+floor2_1rect = floor2_1img.get_rect(topleft=(-530, HEIGHT - floor2_1img.get_height() + 70))
+floor2_3rect = floor2_3img.get_rect(topleft=(-100, HEIGHT - floor2_3img.get_height() + 67))
+floor2_2rect = floor2_2img.get_rect(topleft=(0, HEIGHT - floor2_2img.get_height() + 67))
 
-floor2_3rect = floor2_3img.get_rect()
-floor2_3rect.x = -100
-floor2_3rect.y = HEIGHT - floor2_3img.get_rect().height + 67
-
-floor2_2rect = floor2_2img.get_rect()
-floor2_2rect.x = 0
-floor2_2rect.y = HEIGHT - floor2_2img.get_rect().height + 67
-
+# физика
 speed = 6
+jump_speed = -19
+gravity = 0.5
+velocity_y = 0
+on_ground = True
 
+# управление1
 move_left = False
 move_right = False
-jump = False
-lives = 5
 
+# жизни/мана
+lives = 5
+soul = 0
+max_soul = 9
+
+# управление2
 while True:
     for event in pg.event.get():
         if event.type == pg.QUIT:
@@ -63,6 +70,10 @@ while True:
             elif event.key == pg.K_a:
                 move_left = True
                 current_sprite = kni_le_sta_img
+            elif event.key == pg.K_SPACE:
+                if on_ground:
+                    velocity_y = jump_speed
+                    on_ground = False
 
         elif event.type == pg.KEYUP:
             if event.key == pg.K_d:
@@ -72,6 +83,7 @@ while True:
                 move_left = False
                 current_sprite = kni_le_sta_img
 
+# право-лево (повороты)
     if move_right:
         if sprite_rect.x + current_sprite.get_rect().width + speed < WIDTH:
             sprite_rect.x += speed
@@ -79,25 +91,35 @@ while True:
         if sprite_rect.x - speed > 0:
             sprite_rect.x -= speed
 
+# вертикальное движение
+    velocity_y += gravity
+    sprite_rect.y += velocity_y
+
+# касания пола (проверка)
+    if sprite_rect.y >= ground_y:
+        sprite_rect.y = ground_y
+        velocity_y = 0
+        on_ground = True
+    else:
+        on_ground = False
+
+
+# рисование
     surface.fill(WHITE)
+
+
+    surface.blit(current_sprite, sprite_rect)
 
     surface.blit(floor2_3img, floor2_3rect)
     surface.blit(floor2_2img, floor2_2rect)
     surface.blit(floor2_1img, floor2_1rect)
     surface.blit(floor_img, floor_rect)
 
-    surface.blit(current_sprite, sprite_rect)
+
 
 
     pg.display.update()
     clock.tick(FPS)
-
-
-
-
-
-
-
 
 
 
