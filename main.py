0e489f59e7324a190d7a12cc26a346a9f13948b4
Вадим <vdpodolski@gmail.com>
2025-06-17 17:00:14 +0300

    Пробую сделать 1 локацию + управление из Hollow Knight в PyGame.  №2 1)   1. Добавил Pygame и создал начальное окно. 2)   1. Добавил спрайт.   2. Добавил передвижение персонажа(пока не проверял со спрайтом). 3)   1. Проверил: персонаж не появляется.   2. В прошлой версии был чёрный фон - теперь белый. 4)   1. Исправил весь код теперь он работает: спрайтом можно управлять вверх, вниз, влево и вправо(пока у него нет пола). 5)   1. Исправил спрайт, теперь всё работает. 6)   1. Добавил 2 спрайт.   2. Пытаюсь сделать так, чтобы в зависимости от нажатия клавиш он применял разные спрайты для одного персонажа(пока не получается).  7)   1. исправил код и теперь персонаж может двигаться вправо влево и изменяются спрайты.  8)   1. Исправил недочёт, когда при окончании передвижения он поворачивался в противоположную сторону.   2. Добавил спрайт пола.   3. ДОбавил сам пол и попытался сделать его бесконечным(пока не получилось). 9)   1. Добавил 3 спрайта пола и сделал их за экраном. 10)   1. Начал делать прыжок.   2. Спрайт чуть чуть поднимается, но пока не опускается.   3. Доделал прыжок + гравитация при падении.   4. Всё подписал. 11)   1. Добавил спрайты атак(работающих пока два).   2. сделал атаку +-.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame as pg
 from random import randint
-
+import time
 
 pg.init()
 pg.font.init()
@@ -8,21 +8,25 @@ clock = pg.time.Clock()
 
 # настройки
 WIDTH, HEIGHT = 1800, 1000
-WHITE = (255, 255, 255)
+BLUE = (27, 32, 39)
 BLACK = (0, 0, 0)
 GREEN = (34, 177, 76)
 YELLOW = (255, 242, 0)
 FPS = 60
 FONT = pg.font.SysFont('Arial', 35)
 surface = pg.display.set_mode((WIDTH, HEIGHT))
+start_time = time.time()
+cd_start_time = 0
 
 # загрузка спрайтов
 kni_le_sta_img = pg.image.load('images/knight/left/knight_stand_l.png').convert_alpha()
 kni_ri_sta_img = pg.image.load('images/knight/right/knight_stand_r.png').convert_alpha()
-floor_img = pg.image.load('images/no knight/floor/floor1.png')
-floor2_2img = pg.image.load('images/no knight/floor/floor2_2.png')
-floor2_1img = pg.image.load('images/no knight/floor/floor2_1.png')
-floor2_3img = pg.image.load('images/no knight/floor/floor2_3.png')
+attack1_le = pg.image.load('images/knight/attack/attack1_le.png').convert_alpha()
+attack1_ri = pg.image.load('images/knight/attack/attack1_ri.png').convert_alpha()
+floor_img = pg.image.load('images/no knight/floor/floor1.png').convert_alpha()
+floor2_2img = pg.image.load('images/no knight/floor/floor2_2.png').convert_alpha()
+floor2_1img = pg.image.load('images/no knight/floor/floor2_1.png').convert_alpha()
+floor2_3img = pg.image.load('images/no knight/floor/floor2_3.png').convert_alpha()
 
 # спрайт персонажа
 current_sprite = kni_ri_sta_img
@@ -48,8 +52,15 @@ velocity_y = 0
 on_ground = True
 
 # управление1
+dam_at1 = 5
+attack = False
 move_left = False
 move_right = False
+
+# Настройки времени атаки
+at_cd = 0
+cd_go = 0.2
+at_go = 0.1
 
 # жизни/мана
 lives = 5
@@ -83,6 +94,34 @@ while True:
                 move_left = False
                 current_sprite = kni_le_sta_img
 
+        if event.type == pg.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                if at_cd == 0:
+                    attack = True
+                    cd_start_time = time.time()
+                    if current_sprite == kni_ri_sta_img:
+                        b = kni_ri_sta_img
+                        current_sprite = attack1_ri
+                    elif current_sprite == kni_le_sta_img:
+                        b = kni_le_sta_img
+                        current_sprite = attack1_le
+
+            if at_cd > 0:
+                elapsed_time = time.time() - cd_start_time
+
+                if elapsed_time >= cd_go:
+                    at_cd = 0
+                    if move_right:
+                        current_sprite = kni_ri_sta_img
+                    elif move_left:
+                        current_sprite = kni_le_sta_img
+                    else:
+                        current_sprite = kni_ri_sta_img
+
+
+                if elapsed_time >= at_go:
+                    attack = False
+
 # право-лево (повороты)
     if move_right:
         if sprite_rect.x + current_sprite.get_rect().width + speed < WIDTH:
@@ -90,6 +129,10 @@ while True:
     elif move_left:
         if sprite_rect.x - speed > 0:
             sprite_rect.x -= speed
+
+    # Отображение cooldown'а атаки
+    if at_cd > 0:
+        elapsed_time = time.time() - cd_start_time
 
 # вертикальное движение
     velocity_y += gravity
@@ -103,11 +146,10 @@ while True:
     else:
         on_ground = False
 
-
 # рисование
-    surface.fill(WHITE)
+    surface.fill(BLUE)
 
-
+# Отрисовка спрайтов
     surface.blit(current_sprite, sprite_rect)
 
     surface.blit(floor2_3img, floor2_3rect)
@@ -115,12 +157,14 @@ while True:
     surface.blit(floor2_1img, floor2_1rect)
     surface.blit(floor_img, floor_rect)
 
-
-
+# Отображение жизней и маны
+    lives_text = FONT.render(f"Lives: {lives}", True, BLACK)
+    soul_text = FONT.render(f"Soul: {soul}/{max_soul}", True, BLACK)
+    surface.blit(lives_text, (10, 10))
+    surface.blit(soul_text, (10, 50))
 
     pg.display.update()
     clock.tick(FPS)
-
 
 
 
